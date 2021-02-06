f78b623b022be964a3774fb860be5a3da3810a84
stgbs <avbelov86@mail.ru>
2021-02-06 18:55:54 +0300

    task_1

---
+++ lesson_3/les3_task1.py
@@ -0,0 +1,22 @@
+"""lesson_3 task_1"""
+
+
+# Реализовать функцию, принимающую два числа (позиционные аргументы) и
+# выполняющую их деление. Числа запрашивать у пользователя,
+# предусмотреть обработку ситуации деления на ноль.
+
+def my_func(s_1, s_2):
+    try:
+        result = s_1 / s_2
+        return int(result) if result.is_integer() else round(result, 3)
+    except ZeroDivisionError:
+        print('Делить на ноль нельзя!')
+
+
+try:
+    num_1 = float(input('Введите первое число: '))
+    num_2 = float(input('Введите второе число: '))
+except ValueError:
+    print("Необходимо ввести число!")
+
+print(my_func(num_1, num_2))
