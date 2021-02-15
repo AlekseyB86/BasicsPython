bbd47c798ff562f0a5663dda5b616448a44d4abb
stgbs <avbelov86@mail.ru>
2021-02-15 23:18:41 +0300

    init task6

---
+++ lesson_5/les5_task6.py
@@ -0,0 +1,18 @@
+"""lesson5_task6"""
+# Необходимо создать (не программно) текстовый файл, где каждая строка
+# описывает учебный предмет и наличие лекционных, практических и
+# лабораторных занятий по этому предмету и их количество.
+# Важно, чтобы для каждого предмета не обязательно были все типы занятий.
+# Сформировать словарь, содержащий название предмета и общее количество
+# занятий по нему. Вывести словарь на экран.
+from pprint import pprint
+
+with open("text_6.txt", "r", encoding="utf-8") as f_obj:
+    content = [stroke.strip() for stroke in f_obj]
+chars_r = ["-", "(пр)", "(л)", "(лаб)", ":"]
+for ch in chars_r:
+    content = [el.replace(ch, "") for el in content]
+content = [el.split() for el in content]
+content = [[el[0], sum([int(i) for i in el if i.isdigit()])] for el in content]
+content_dict = {el[0]: el[1] for el in content}
+pprint(content_dict, sort_dicts=False)
